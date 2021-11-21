3dba0341eb3a1f50561d268d4e9d384143db947b
Melanie Turek <melanie@derrabus.de>
2021-11-21 14:35:02 +0100

    Add css animation to pads

--- src/app.js
+++ src/app.js
@@ -1,36 +1,41 @@
 class Drumkit {
-    constructor() {
-        this.pads = document.querySelectorAll('.pad');
-        this.playBtn = document.querySelector('.play');
-        this.kickAudio = document.querySelector('.kick-sound');
-        this.snareAudio = document.querySelector('.snare-sound');
-        this.hihatAudio = document.querySelector('.hihat-sound');
-        this.index = 0;
-        this.bpm = 150;
-    }
-    activePad() {
-        this.classList.toggle('active');
-    }   
-    repeat() {
-        let step = this.index % 8;
-        const activeBars = document.querySelectorAll(`.b${step}`);
-        this.index++;
-    }
-    start() {
-        const interval = (60 / this.bpm) * 1000;
-        setInterval(() => {
-            this.repeat();
-        }, interval);
-    }
+  constructor() {
+    this.pads = document.querySelectorAll('.pad');
+    this.playBtn = document.querySelector('.play');
+    this.kickAudio = document.querySelector('.kick-sound');
+    this.snareAudio = document.querySelector('.snare-sound');
+    this.hihatAudio = document.querySelector('.hihat-sound');
+    this.index = 0;
+    this.bpm = 150;
+  }
+  activePad() {
+    this.classList.toggle('active');
+  }
+  repeat() {
+    let step = this.index % 8;
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    activeBars.forEach(bar => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+    });
+    this.index++;
+  }
+  start() {
+    const interval = (60 / this.bpm) * 1000;
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
 }
 
 const drumKit = new Drumkit();
 
 drumKit.pads.forEach(pad => {
-    pad.addEventListener('click', drumKit.activePad);
+  pad.addEventListener('click', drumKit.activePad);
+  pad.addEventListener('animationend', function() {
+    this.style.animation = '';
+  });
 });
 
 drumKit.playBtn.addEventListener('click', function() {
-    drumKit.start();
+  drumKit.start();
 });
-    
